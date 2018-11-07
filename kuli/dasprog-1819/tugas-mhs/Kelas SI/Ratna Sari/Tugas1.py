739fbaab701594123056cac113d3b489557bf9c5
ratnasari13 <44794582+ratnasari13@users.noreply.github.com>
2018-11-07 09:47:14 +0700

    Tugas 1

---
+++ kuli/dasprog-1819/tugas-mhs/Kelas SI/Ratna Sari/Tugas1.py
@@ -0,0 +1,17 @@
+Saya = input('Masukkan berat badan anda {kg}')
+Rekan = input('masukkan berat badan rekan anda {kg}')
+Rekan = int(Rekan)
+Saya = int(Rekan)
+Hasil1 = Rekan - Saya
+Hasil2 = saya - Rekan
+
+if Rekan > Saya:
+    Kategori = 'Anda lebih ringan' int(Hasil1), 'kg dari pada Rekan anda'
+    
+elif Saya > Rekan :
+    kategori = 'Anda lebih berat', int(Hasil2). 'kg dari rekan anda'
+    
+else:
+    kategori = 'Berat badan anda sama dengan rekan anda'
+    
+print  ('{}'.format (kategori))    
